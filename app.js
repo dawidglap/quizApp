845826795a4e8d34ac1f469823d1b92586c8cc8e
Dawid Glapiak <dg18@live.it>
2022-02-12 16:45:57 +0100

    card almost done

--- app.js
+++ app.js
@@ -1,5 +1,50 @@
 const quizData = [
     {
-        questions
-    }
+        question: `Quale è il mio cibo preferito?`,
+        a: `Pizza`,
+        b: `Pasta`,
+        c: `Pierogi`,
+        d: `Tofu`,
+        correct: `c`,
+    },
+
+    {
+        question: `Quanti anni ho?`,
+        a: `25`,
+        b: `40`,
+        c: `31`,
+        d: `110`,
+        correct: `c`,
+        
+    },
+
+    {
+        question: `Quale colore mi piace di più?`,
+        a: `Giallo`,
+        b: `Arancione`,
+        c: `Blu`,
+        d: `Verde`,
+        correct: `c`,
+    },
+
+    {
+        question: `Quale è il mio habitat?`,
+        a: `Mare`,
+        b: `Montagna`,
+        c: `Marte`,
+        d: `Mongolia`,
+        correct: `a`,
+    },
+    
+    {
+        question: `Dove ho conosciuto il mio primo e unico amore?`,
+        a: `Minnesota`,
+        b: `Moai Beach`,
+        c: `Frigorifero`,
+        d: `Groenlandia`,
+        correct: `b`,
+    },
+
+
+
 ]
